847ebd2e91dc1447175069b9d2461130231186c9
Uzair-Ul-Hassan <Uzair-Ul-Hassan@users.noreply.github.com>
2025-12-31 10:22:21 +0000

    deploy: 7bb96a078c37c084bbd48e8677c4cdae9fd80697

---
+++ _next/static/chunks/app/electrical/page-ebab7fc980225239.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[127,286,158,813,826,615,71,542,400,757,537],{3785:function(n,e,u){Promise.resolve().then(u.t.bind(u,8173,23))}},function(n){n.O(0,[173,971,23,744],function(){return n(n.s=3785)}),_N_E=n.O()}]);
